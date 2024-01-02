93568474b29c0f30305e564afdf61814ec4fa849
weisheng <weisheng_rules@hotmail.com>
2024-01-02 23:39:29 +0800

    added some notes for section 12

--- section-12/quiz-app/src/components/Answers.jsx
+++ section-12/quiz-app/src/components/Answers.jsx
@@ -3,6 +3,11 @@ import { useRef } from "react";
 // Answers has it's own component so that shuffledAnswers will always be destroyed and recreated on a new question using "key" in the parent component
 export default function Answers({answers, selectedAnswer, answerState, onSelect}) {
     const correctAnswerText = answers[0];
+
+    // based on my understanding, useRef can also be used to make sure a variable holds its value 
+    // so that when a state changes and the component rerenders, it doesn't get overwritten
+    // shuffledAnswers is using useRef because without it, the answers will get shuffled again and again whenever the user selects an answer and the state changes
+    // causing the component to rerender and creating a new shuffledAnswers array
     const shuffledAnswers = useRef();
 
     if (!shuffledAnswers.current) {
